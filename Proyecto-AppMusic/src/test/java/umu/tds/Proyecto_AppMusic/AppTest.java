289e8a6051d6da9b339ad8afbb178aafbad1fc94
Juan Garcia <juangarciaalos2@gmail.com>
2023-11-27 11:59:37 +0100

    Primer commit

---
+++ Proyecto-AppMusic/src/test/java/umu/tds/Proyecto_AppMusic/AppTest.java
@@ -0,0 +1,20 @@
+package umu.tds.Proyecto_AppMusic;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
